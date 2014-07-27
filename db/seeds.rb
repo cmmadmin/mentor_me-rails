e6b219d1ecf13979f23ae5fbf02ed8c5a2b4cd5f
Paul Strong <lapluviosilla@gmail.com>
2014-07-26 22:35:40 -0500

    Seed data fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,38 +16,38 @@ curriculum_path = File.expand_path("../curriculum.tsv", __FILE__)
 
 
 edition = Edition.where(name: 'CMT College Edition', code: 'college').first_or_create
-# edition.create_surveys
-# edition.create_lifelist
-# edition.create_curriculums
+edition.create_surveys
+edition.create_lifelist
+edition.create_curriculums
 
-# selfAssessGroups = %w(Physical Emotional Spiritual Finances)
-# selfAssessGroups += ["Relationships (Friends)", "Relationships (Romantic)", "Relationships (Parents)"]
-# selfAssessGroups += %w(Meaning/Purpose)
-# selfAssessGroups.each do |g|
-#   QuestionGroup.where(title: g, survey_id: edition.snapshot_self_assessment_survey.id).first_or_create
-# end
+selfAssessGroups = %w(Physical Emotional Spiritual Finances)
+selfAssessGroups += ["Relationships (Friends)", "Relationships (Romantic)", "Relationships (Parents)"]
+selfAssessGroups += %w(Meaning/Purpose)
+selfAssessGroups.each do |g|
+  QuestionGroup.where(title: g, survey_id: edition.snapshot_self_assessment_survey.id).first_or_create
+end
 
-# edition.snapshot_interactive_survey.create_default_question_group
-# edition.snapshot_observations_survey.create_default_question_group
+edition.snapshot_interactive_survey.create_default_question_group
+edition.snapshot_observations_survey.create_default_question_group
 
-# CSV.foreach(selfassess_path, headers: true) do |row|
-#   hash = row.to_hash
-#   hash.keep_if{|key, value| %w(question_group body question_type).include? key}
-#   group = QuestionGroup.where(title: hash['question_group'], survey_id: edition.snapshot_self_assessment_survey.id).first
-#   Question.where(question_group_id: group.id, body: hash['body'], question_type: hash['question_type']).first_or_create if group
-# end
+CSV.foreach(selfassess_path, headers: true) do |row|
+  hash = row.to_hash
+  hash.keep_if{|key, value| %w(question_group body question_type).include? key}
+  group = QuestionGroup.where(title: hash['question_group'], survey_id: edition.snapshot_self_assessment_survey.id).first
+  Question.where(question_group_id: group.id, body: hash['body'], question_type: hash['question_type']).first_or_create if group
+end
 
-# CSV.foreach(interactive_qa_path, headers: true) do |row|
-#   hash = row.to_hash
-#   hash.keep_if{|key, value| %w(body question_type).include? key}
-#   Question.where(body: hash['body'], question_type: hash['question_type'], question_group_id: edition.snapshot_interactive_survey.default_question_group.id).first_or_create
-# end
+CSV.foreach(interactive_qa_path, headers: true) do |row|
+  hash = row.to_hash
+  hash.keep_if{|key, value| %w(body question_type).include? key}
+  Question.where(body: hash['body'], question_type: hash['question_type'], question_group_id: edition.snapshot_interactive_survey.default_question_group.id).first_or_create
+end
 
-# CSV.foreach(observe_path, headers: true) do |row|
-#   hash = row.to_hash
-#   hash.keep_if{|key, value| %w(body question_type).include? key}
-#   Question.where(body: hash['body'], question_type: hash['question_type'], question_group_id: edition.snapshot_observations_survey.default_question_group.id).first_or_create
-# end
+CSV.foreach(observe_path, headers: true) do |row|
+  hash = row.to_hash
+  hash.keep_if{|key, value| %w(body question_type).include? key}
+  Question.where(body: hash['body'], question_type: hash['question_type'], question_group_id: edition.snapshot_observations_survey.default_question_group.id).first_or_create
+end
 
 
 item = {}
@@ -57,7 +57,7 @@ develop_category = develop_goal = develop_item = nil
 CSV.foreach(curriculum_path, {headers: [:category, :goal_title, :goal_description, :label, :item_description, :item_link], col_sep: "\t", skip_blanks: true, quote_char: 4.chr}) do |row|
   next if $. == 1 # Skip header line
   # Incorporate any values that have changed
-  item_hash = row.to_hash.delete_if{|k,v| v.nil? && ![:label, :item_description, :item_link].include?(k) }
+  item_hash = row.to_hash.delete_if{|k,v| v.nil? && ![:item_description, :item_link].include?(k) }
   item = item_hash.reverse_merge item
   
   develop_category = DevelopCategory.find_or_create_by_title(item[:category]) if item_hash[:category]
